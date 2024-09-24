ae475486ba3ae36dc0cf834470bbd686bb65f552
imihirjain <jmanav251@gmail.com>
2024-09-24 15:26:58 +0530

    images done

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // Import images from the assets folder
-import image2 from "../assets/2.png";
-import image3 from "../assets/3.png";
-import image4 from "../assets/4.png";
+import image2 from "../assets/2.JPG";
+import image3 from "../assets/3.JPG";
+import image4 from "../assets/4.JPG";
 import { Link } from "react-router-dom";
 
 const Discover = () => {
